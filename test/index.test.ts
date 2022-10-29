6736a12522963fd083c984d63a6c541c15e10727
Ray Foss <ray@vblip.com>
2022-10-28 22:08:10 -0500

    update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,13 @@
 import adapterTests from '@feathersjs/adapter-tests'
 import errors from '@feathersjs/errors'
-import feathers from '@feathersjs/feathers'
+import { feathers } from '@feathersjs/feathers'
 // TODO: use 'assert/strict'
 import assert from 'node:assert'
 // import test from 'node:test' // Node 18
 import test from 'node-test'
 
-import memory from '../src/index.js'
+import { MemoryService } from '../src'
+
 const testSuite = adapterTests([
   '.options',
   '.events',
@@ -16,27 +17,39 @@ const testSuite = adapterTests([
   '._update',
   '._patch',
   '._remove',
+  '.$get',
+  '.$find',
+  '.$create',
+  '.$update',
+  '.$patch',
+  '.$remove',
   '.get',
   '.get + $select',
   '.get + id + query',
   '.get + NotFound',
   '.get + id + query id',
   '.find',
+  '.find + paginate + query',
   '.remove',
   '.remove + $select',
   '.remove + id + query',
   '.remove + multi',
+  '.remove + multi no pagination',
   '.remove + id + query id',
   '.update',
   '.update + $select',
   '.update + id + query',
   '.update + NotFound',
   '.update + id + query id',
+  '.update + query + NotFound',
   '.patch',
   '.patch + $select',
   '.patch + id + query',
   '.patch multiple',
-  '.patch multi query',
+  '.patch multiple no pagination',
+  '.patch multi query same',
+  '.patch multi query changed',
+  '.patch + query + NotFound',
   '.patch + NotFound',
   '.patch + id + query id',
   '.create',
@@ -69,20 +82,44 @@ const testSuite = adapterTests([
   '.find + paginate',
   '.find + paginate + $limit + $skip',
   '.find + paginate + $limit 0',
-  '.find + paginate + params'
+  '.find + paginate + params',
+  'params.adapter + paginate',
+  'params.adapter + multi'
 ])
+
 test('Feathers Memory Service', async (t) => {
+  type Person = {
+    id: number
+    name: string
+    age: number
+  }
+
+  type Animal = {
+    type: string
+    age: number
+  }
+
   const events = ['testing']
-  const app = feathers()
-    .use('/people', memory({ events }))
-    .use('/people-customid', memory({
-      id: 'customid', events
-    }))
+  const app = feathers<{
+    people: MemoryService<Person>
+    'people-customid': MemoryService<Person>
+    animals: MemoryService<Animal>
+    matcher: MemoryService
+  }>()
+
+  app.use('people', new MemoryService<Person>({ events }))
+  app.use(
+    'people-customid',
+    new MemoryService<Person>({
+      id: 'customid',
+      events
+    })
+  )
+
 
   await t.test('is CommonJS compatible', () =>
     assert.strictEqual(typeof module.require('../src/index.js'), 'function')
   )
-
   await t.test('update with string id works', async () => {
     const people = app.service('people')
     const person = await people.create({
@@ -90,7 +127,7 @@ test('Feathers Memory Service', async (t) => {
       age: 33
     })
 
-    const updatedPerson = await people.update(person.id.toString(), person)
+    const updatedPerson: any = await people.update(person.id.toString(), person)
 
     assert.strictEqual(typeof updatedPerson.id, 'number')
 
@@ -98,15 +135,18 @@ test('Feathers Memory Service', async (t) => {
   })
 
   await t.test('patch record with prop also in query', async () => {
-    app.use('/animals', memory({ multi: true }))
+    app.use('animals', new MemoryService<Animal>({ multi: true }))
     const animals = app.service('animals')
-    await animals.create([{
-      type: 'cat',
-      age: 30
-    }, {
-      type: 'dog',
-      age: 10
-    }])
+    await animals.create([
+      {
+        type: 'cat',
+        age: 30
+      },
+      {
+        type: 'dog',
+        age: 10
+      }
+    ])
 
     const [updated] = await animals.patch(null, { age: 40 }, { query: { age: 30 } })
 
@@ -119,21 +159,24 @@ test('Feathers Memory Service', async (t) => {
     let sorterCalled = false
     let matcherCalled = false
 
-    app.use('/matcher', memory({
-      matcher() {
-        matcherCalled = true
-        return function () {
-          return true
-        }
-      },
+    app.use(
+      'matcher',
+      new MemoryService({
+        matcher() {
+          matcherCalled = true
+          return function () {
+            return true
+          }
+        },
 
-      sorter() {
-        sorterCalled = true
-        return function () {
-          return 0
+        sorter() {
+          sorterCalled = true
+          return function () {
+            return 0
+          }
         }
-      }
-    }))
+      })
+    )
 
     await app.service('matcher').find({
       query: { $sort: { something: 1 } }
@@ -166,15 +209,14 @@ test('Feathers Memory Service', async (t) => {
       name: 'Tester'
     })
     const results = await people.find({
+      paginate: false,
       query: {
         name: 'Tester',
         $select: ['name']
       }
     })
 
-    assert.deepStrictEqual(results[0], { name: 'Tester' },
-      'deepEquals the same'
-    )
+    assert.deepStrictEqual(results[0], { name: 'Tester' }, 'deepEquals the same')
 
     await people.remove(person.id)
   })
@@ -183,8 +225,8 @@ test('Feathers Memory Service', async (t) => {
     try {
       await app.service('people').update(null, {})
       throw new Error('Should never get here')
-    } catch (error) {
-      assert.strictEqual(error.message, 'You can not replace multiple instances. Did you mean \'patch\'?')
+    } catch (error: any) {
+      assert.strictEqual(error.message, "You can not replace multiple instances. Did you mean 'patch'?")
     }
   })
 
